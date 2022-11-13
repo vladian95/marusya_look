4911b9fc3ebc12a7d100826ddff64af84f49ffda
vladian95 <kvankvan1995@gmail.com>
2022-11-13 15:49:41 +0100

    added tilt

--- src/components/job/Job.jsx
+++ src/components/job/Job.jsx
@@ -1,17 +1,41 @@
 import './job.scss';
-import Tilt from 'react-vanilla-tilt';
+import Tilt from 'react-parallax-tilt';
 
 export default function Job() {
   return (
     <div className="Job" id="Job">
+      <h1 className="Job-Title">Виды деятельности</h1>
       <div className="Job-Wrapper">
-        <Tilt className="Card">
-          <div className="Card"></div>
+        <Tilt
+          className="parallax-effect-glare-scale Job-Item-Brow"
+          perspective={500}
+          glareEnable={true}
+          glareMaxOpacity={0.45}
+          scale={1}
+          gyroscope={true}
+        >
+          <a href="#Contact">БРОВИ</a>
         </Tilt>
-        <Tilt className="Card">
-          <div className="Card"></div>
+        <Tilt
+          className="parallax-effect-glare-scale Job-Item-MakeUp"
+          perspective={500}
+          glareEnable={true}
+          glareMaxOpacity={0.45}
+          scale={1}
+          gyroscope={true}
+        >
+          <a href="#Contact">ВИЗАЖ</a>
         </Tilt>
-        <p>asdasdasmnd,amsnd</p>
+        <Tilt
+          className="parallax-effect-glare-scale Job-Item-Sertificate"
+          perspective={500}
+          glareEnable={true}
+          glareMaxOpacity={0.45}
+          scale={1}
+          gyroscope={true}
+        >
+          <a href="#Contact">ОБУЧЕНИЯ</a>
+        </Tilt>
       </div>
     </div>
   );
