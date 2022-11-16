e66f27b9cbcfc5941087c2377f5365419e75448e
vladian95 <kvankvan1995@gmail.com>
2022-11-16 19:05:04 +0100

    done neon

--- src/components/neon/Neon.jsx
+++ src/components/neon/Neon.jsx
@@ -1,7 +1,47 @@
+import NeonAnimatedShapes from '../neonAnimatedShapes/NeonAnimatedShapes';
 import './neon.scss';
 
 function Neon() {
-  return <div className="Neon"></div>;
+  return (
+    <div className="Neon">
+      <h1 className="Neon-Title" data-text="&nbsp;NEON MAKE UP&nbsp;">
+        &nbsp;NEON MAKE UP&nbsp;
+      </h1>
+      <div className="Neon-Wrapper">
+        <div className="Neon-Card Neon-CardOne">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className="Neon-Card Neon-CardTwo">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className="Neon-Card Neon-CardThree">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className="Neon-Card Neon-CardFour">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className="Neon-Card Neon-CardFive">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <NeonAnimatedShapes />
+    </div>
+  );
 }
 
 export default Neon;
---
+++ src/components/neonAnimatedShapes/NeonAnimatedShapes.jsx
@@ -0,0 +1,13 @@
+import './neonAnimatedShapes.scss';
+
+function NeonAnimatedShapes() {
+  return (
+    <>
+      <div className="Neon-Circle"></div>
+      <div className="Neon-Circle-Two"></div>
+      <div className="Neon-Circle-Three"></div>
+    </>
+  );
+}
+
+export default NeonAnimatedShapes;
